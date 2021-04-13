04c05bc92683835474e09b48b63a7c6c099e75cf
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-14 01:46:46 +0400

    ff

--- VHDL2/ff.v
+++ VHDL2/ff.v
@@ -12,16 +12,26 @@ module ff(D,clk,Q,Qbar);
   
   // not notq (Qbar,Q);
 
-  not not1 (Dnot,D);
+  wire o1;
+  wire o2;
+  wire Qo;
+  wire Qbaro;
+
+  not  not1  (Dnot,D);
   nand nand1 (o1,D,clk);
   nand nand2 (o2,Dnot,clk);
   nand nand3 (Q,o1,Qbar);
   nand nand4 (Qbar,o2,Q);
 
+  // always@(posedge clk)
+  // begin
+  //   Q<=Qo;
+  // end
+
   // always @(posedge clk) 
   // begin
   
-  //     Q <= D; 
+  //   Q <= D; 
   // end 
   
 endmodule 
