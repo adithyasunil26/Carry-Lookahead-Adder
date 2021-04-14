683f94126d5a6df3af6c42feb2937c37c1adf543
adithyasunil26 <adithyasunil26@gmail.com>
2021-04-14 20:52:22 +0400

    fftb

--- VHDL2/ff_tb.v
+++ VHDL2/ff_tb.v
@@ -38,11 +38,13 @@ module flipfloptb;
         D=0;
     #10 clk  =~clk;
         D=1;
-        reset=1;
+    #10 clk  =~clk;
+    #10 clk  =~clk;
+    #10 clk  =~clk;
     #10 clk  =~clk;
 
   end 
   
   initial 
-		$monitor("D=%d clk=%d sync_reset=%d Q=%d Qbar=%d",D,clk,reset,Q,Qbar);
+		$monitor("D=%d clk=%d Q=%d Qbar=%d",D,clk,Q,Qbar);
 endmodule
